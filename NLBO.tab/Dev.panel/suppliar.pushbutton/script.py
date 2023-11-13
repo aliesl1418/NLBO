8f7bf2f40088bb0633ced318de78726305f5cf55
aliesl1418 <149317587+aliesl1418@users.noreply.github.com>
2023-11-13 03:48:47 +0330

    update

--- NLBO.tab/Dev.panel/suppliar.pushbutton/script.py
+++ NLBO.tab/Dev.panel/suppliar.pushbutton/script.py
@@ -2,4 +2,26 @@ __title__ = "NLBO suppliar"
 __author__ = "Ali Eslamifar"
 __doc__ = """ This is NLBO supllier application"""
 
+# from pyrevit import forms
+# items = ['item1', 'item2', 'item3']
+# forms.SelectFromList.show(items, button_name='Select Item')
 
+
+import clr
+clr.AddReference('System.windows.Forms')
+clr.AddReference('IronPython.Wpf')
+
+#find the path of ui.xaml
+from pyrevit import script
+xamlfile = script.get_bundle_file('ui.xaml')
+
+#import wpf creator and base window
+import wpf
+from System import Windows
+
+class MyWindow(Windows.Window):
+    def __init__(self):
+        wpf.LoadComponent(self, xamlfile)
+
+# let's show the windows
+MyWindow().ShowDialog()
