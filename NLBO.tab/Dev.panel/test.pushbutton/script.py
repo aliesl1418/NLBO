193b8e6761c3032135854436f709e4febe740031
aliesl1418 <149317587+aliesl1418@users.noreply.github.com>
2023-11-20 02:26:49 +0330

    updated

--- NLBO.tab/Dev.panel/test.pushbutton/script.py
+++ NLBO.tab/Dev.panel/test.pushbutton/script.py
@@ -26,7 +26,7 @@ __doc__ = """ This is NLBO supllier application"""
 #         print(param_name, param_value)
 
 
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, ViewSchedule
+
 #
 # doc = __revit__.ActiveUIDocument.Document
 #
@@ -143,7 +143,49 @@ from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, ViewSch
 #
 # print(elements)
 
+# #first_try
+# from pyrevit import revit,DB
+# from Autodesk.Revit.DB import FilteredElementCollector, ParameterValueProvider, FilterStringEquals, ElementParameterFilter,BuiltInCategory
+# import clr
+# clr.AddReference('RevitAPI')
+# doc = __revit__.ActiveUIDocument.Document
+# uidoc = __revit__.ActiveUIDocument
+# collector = FilteredElementCollector(doc)
+# elements = collector.OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElementIds()
+#
+# selection = [doc.GetElement(x) for x in elements]
+# for element in selection:
+#  param = element.LookupParameter('Unconnected Height')
+#  print(param)
 
 
+# #second try
+# from pyrevit import revit,DB
+# from Autodesk.Revit.DB import FilteredElementCollector, ParameterValueProvider, FilterStringEquals, ElementParameterFilter,BuiltInCategory,BuiltInParameter
+# import clr
+# clr.AddReference('RevitAPI')
+# doc = __revit__.ActiveUIDocument.Document
+# collector = FilteredElementCollector(doc)
+# elements = collector.OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElementIds()
+#
+# selection = [doc.GetElement(x) for x in elements]
+# for element in selection:
+#  param = element.get_Parameter(BuiltInParameter.WALL_BASE_CONSTRAINT).AsValueString()
+#  print(param)
 
+#third try
+import System
+from pyrevit import revit,DB
+from Autodesk.Revit.DB import FilteredElementCollector, ParameterValueProvider, FilterStringEquals, ElementParameterFilter,BuiltInCategory,BuiltInParameter
+import clr
+clr.AddReference('RevitAPI')
+doc = __revit__.ActiveUIDocument.Document
+collector = FilteredElementCollector(doc)
+elements = collector.OfCategory(BuiltInCategory.OST_Doors).WhereElementIsNotElementType().ToElementIds()
 
+selection = [doc.GetElement(x) for x in elements]
+guid_str = 'afec3538-445f-49e7-b8cd-2e852da79350'
+guid = System.Guid(guid_str)
+for element in selection:
+ param = element.get_Parameter(guid).AsValueString()
+ print(param)
